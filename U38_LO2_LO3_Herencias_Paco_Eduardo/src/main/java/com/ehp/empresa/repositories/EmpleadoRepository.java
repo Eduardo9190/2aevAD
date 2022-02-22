1aaace075d6b8231afd04ffe1f3f0233b8d03be8
Eduardo Herencias Paco <73492355+Eduardo9190@users.noreply.github.com>
2022-02-22 16:24:04 +0100

    Update EmpleadoRepository.java

--- U38_LO2_LO3_Herencias_Paco_Eduardo/src/main/java/com/ehp/empresa/repositories/EmpleadoRepository.java
+++ U38_LO2_LO3_Herencias_Paco_Eduardo/src/main/java/com/ehp/empresa/repositories/EmpleadoRepository.java
@@ -4,6 +4,7 @@ import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.ehp.empresa.entities.Empleado;
 
+@Repository
 public interface EmpleadoRepository extends JpaRepository<Empleado, String>{
 
 }
